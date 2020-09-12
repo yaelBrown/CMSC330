c97f224bdc2d4f722c4ee4dd4a1f5cd5ecfe4e85
Yael Brown <yaelrbrown@gmail.com>
2020-09-12 01:20:02 -0500

    finished assignment i guess

--- src/proj/Project1/GUI.java
+++ src/proj/Project1/GUI.java
@@ -5,8 +5,9 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class GUI extends JFrame {
-    public GUI() {}
     public GUI(ArrayList<String> gram) {
+        this.frame = new JFrame();
+        this.panel = new JPanel();
         guiParser(gram);
         this.frame.setVisible(true);
     }
@@ -15,13 +16,14 @@ public class GUI extends JFrame {
     JPanel panel;
 
     public void guiParser(ArrayList<String> d) {
-        for (String s : d) {
-            guiParserHelper(s);
-        }
+        guiParserHelper(d, 0, this.panel);
     }
 
-    private void guiParserHelper(String s) {
-        String[] line  = s.split(" ");
+    private void guiParserHelper(ArrayList<String> a, int idx, JPanel pnl) {
+        if (idx >= a.size()) {
+            return;
+        }
+        String[] line  = a.get(idx).split(" ");
         System.out.println("line[0] = " + line[0]);
         switch (line[0]) {
             case "Window":
@@ -33,39 +35,61 @@ public class GUI extends JFrame {
                 this.frame.setSize(width, height);
                 this.frame.setLocationRelativeTo(null);
                 this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                if (line[4].equals("Layout")) {
+                    if (line[5].equals("Flow:")) {
+                        FlowLayout fl = new FlowLayout();
+                        fl.setAlignment(FlowLayout.TRAILING);
+                        pnl.setLayout(fl);
+                        this.frame.add(pnl);
+                    } else {
+                        pnl = new JPanel();
+                    }
+                }
                 break;
             case "Panel":
-                this.panel = new JPanel();
+                // this.frame.add(pnl);
 
                 int row = Integer.parseInt(line[2].substring(5, endVal(line[2])));
                 int col = Integer.parseInt(line[3].substring(0, endVal(line[3])));
                 int hgap = Integer.parseInt(line[4].substring(0, endVal(line[4])));
                 int vgap = Integer.parseInt(line[5].substring(0, endVal(line[5]) - 1));
 
-                this.panel.setLayout(new GridLayout(row, col, hgap, vgap));
-                this.frame.add(this.panel);
+                JPanel tempPnl = new JPanel();
+                tempPnl.setLayout(new GridLayout(row, col, hgap, vgap));
+
                 break;
             case "Textfield":
-                JTextField tf = new JTextField(line[1]);
-                this.frame.add(tf);
+                if (line[1].equals("\"\";")) {
+                    JTextField tf = new JTextField();
+                    pnl.add(tf);
+                } else {
+                    JTextField tf = new JTextField("", Integer.parseInt(line[1].substring(0, line[1].length() - 1)));
+                    pnl.add(tf);
+                }
                 break;
             case "Label":
-                Label l = new Label(line[1]);
-                this.frame.add(l);
+                if (line[1].equals("\"\";")) {
+                    Label l = new Label();
+                    pnl.add(l);
+                } else {
+                    Label l = new Label(line[1]);
+                    pnl.add(l);
+                }
                 break;
             case "Button":
-     -           Button b = new Button(line[1].substring(1, endVal(line[1]) - 1));
-                this.panel.add(b);
+                Button b = new Button(line[1].substring(1, endVal(line[1]) - 1));
+                pnl.add(b);
                 b = null;
+                break;
+            case "End;":
                 break;
             default:
                 break;
         }
-        return;
+        guiParserHelper(a, idx + 1, pnl);
     }
 
     private int endVal(String s) {
         return s.length() - 1;
     }
-
 }
